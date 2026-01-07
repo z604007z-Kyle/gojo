83a5c8d63f824d88d4ff46df3c8e70d10b5a6f8b
z604007z-Kyle <z604007z@gmail.com>
2026-01-07 11:33:11 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
 
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = target / speed;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    // 這裡可以結合 Intersection Observer 偵測捲動到位置才啟動
+    updateCount();
+});
